7b7162e6ead4f51ada51c50e840570c524de99ea
eliandev <geordymemdoza@gmail.com>
2024-12-09 17:16:15 -0600

    Add hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,10 +4,10 @@ export const Hero = () => {
   return (
     <section className="flex bg-white px-10 py-10 rounded-md mt-10">
       <div className="h-full flex flex-col justify-center">
-        <h1 className="text-[54px] font-bold ">
+        <h1 className="text-[54px] font-bold leading-[60px]">
           ¡Despierta tu creatividad y transforma tu pasión en arte!
         </h1>
-        <p>
+        <p className="my-4">
           Explora nuestros cursos de diseño, dibujo y técnicas artísticas para
           llevar tus habilidades al siguiente nivel. Aprende a tu propio ritmo,
           con contenido diseñado para inspirarte y potenciar tu talento.
